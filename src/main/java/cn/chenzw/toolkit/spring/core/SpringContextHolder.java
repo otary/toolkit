fb26c2e9b2ca1c6b34dd28ee037a751bdd88e035
chenzw <chenzw@ffcs.cn>
2019-08-15 16:38:58 +0800

    initialization

--- src/main/java/cn/chenzw/toolkit/spring/core/SpringContextHolder.java
+++ src/main/java/cn/chenzw/toolkit/spring/core/SpringContextHolder.java
@@ -1,6 +1,7 @@
 package cn.chenzw.toolkit.spring.core;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -10,19 +11,28 @@ import org.springframework.web.context.ContextLoader;
  * @author chenzw
  */
 @Component
-public class SpringContextHolder implements ApplicationContextAware {
+public class SpringContextHolder implements ApplicationContextAware, DisposableBean {
 
     private static ApplicationContext appContext;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        appContext = applicationContext;
-    }
 
     public static ApplicationContext getAppContext() {
         if (appContext == null) {
             appContext = ContextLoader.getCurrentWebApplicationContext();
         }
         return appContext;
+    }
+
+    public static void clear() {
+        appContext = null;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appContext = applicationContext;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        clear();
     }
 }
