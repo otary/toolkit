0ec7ab4dbef3594808d0ad071d2cc7301c562238
chenzw <656469722@qq.com>
2019-08-18 21:02:07 +0800

    initialization

---
+++ src/main/java/cn/chenzw/toolkit/datasource/entity/ColumnMetaData.java
@@ -0,0 +1,117 @@
+package cn.chenzw.toolkit.datasource.entity;
+
+public class ColumnMetaData {
+
+    /**
+     * 字段名称
+     */
+    private String columnName;
+
+    /**
+     * 字段类型名称
+     */
+    private String typeName;
+
+    /**
+     * 字段大小
+     */
+    private String columnSize;
+
+    /**
+     * 字段备注
+     */
+    private String remarks;
+
+    /**
+     * 是否为主键
+     */
+    private Boolean parmaryKey = false;
+
+    /**
+     * 是否为外键
+     */
+    private Boolean foreignKey = false;
+
+    /**
+     * 是否允许为空
+     */
+    private Boolean nullable = false;
+
+    /**
+     * 默认值
+     */
+    private String columnDef;
+
+    /**
+     * 小数部分的位数
+     */
+    private String decimalDigits;
+
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getColumnSize() {
+        return columnSize;
+    }
+
+    public void setColumnSize(String columnSize) {
+        this.columnSize = columnSize;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public Boolean getParmaryKey() {
+        return parmaryKey;
+    }
+
+    public void setParmaryKey(Boolean parmaryKey) {
+        this.parmaryKey = parmaryKey;
+    }
+
+
+    public Boolean getNullable() {
+        return nullable;
+    }
+
+    public void setNullable(Boolean nullable) {
+        this.nullable = nullable;
+    }
+
+    public String getColumnDef() {
+        return columnDef;
+    }
+
+    public void setColumnDef(String columnDef) {
+        this.columnDef = columnDef;
+    }
+
+    public String getDecimalDigits() {
+        return decimalDigits;
+    }
+
+    public void setDecimalDigits(String decimalDigits) {
+        this.decimalDigits = decimalDigits;
+    }
+
+
+}
